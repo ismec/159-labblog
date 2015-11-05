608e35a9d31a495981e2c77ffcfd2b88bbf57ce9
saasbook <saasbook@saasbook.(none)>
2015-11-05 09:21:42 -0400

    My blog edit view created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	def index
+	    @posts = Post.all
 	end
 	def show
 		@post = Post.find(params[:id])
@@ -16,9 +17,12 @@ class PostsController < ApplicationController
 		end
 	end
 	def edit
+		@post = Post.find(params[:id])
 	end 
 	def update
+		
 	end
 	def destroy
+		
 	end
 end
