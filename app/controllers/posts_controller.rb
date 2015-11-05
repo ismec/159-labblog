730159991d3514e636342f7226a79b9160047020
saasbook <saasbook@saasbook.(none)>
2015-11-05 09:23:30 -0400

    My blog update action created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,13 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end 
 	def update
-		
+		@post = Post.find(params[:id])
+ 
+		 if @post.update(post_params)
+		   redirect_to @post
+		 else
+		   render 'edit'
+		 end
 	end
 	def destroy
 		
