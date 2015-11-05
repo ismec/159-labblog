93e9008e3fc3bb403a92ced27ff0878b929ce6e8
saasbook <saasbook@saasbook.(none)>
2015-11-05 09:24:49 -0400

    My blog destroy action created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,9 @@ class PostsController < ApplicationController
 		 end
 	end
 	def destroy
+		@article = Article.find(params[:id])
+  		@article.destroy
+  		redirect_to articles_path
 		
 	end
 end
