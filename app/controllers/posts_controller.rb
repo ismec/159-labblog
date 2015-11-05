48661034a6a05a31ac281d483b9eae06b004e439
saasbook <saasbook@saasbook.(none)>
2015-11-05 08:08:10 -0400

    My blog create action created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,12 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 	def create
+		@post = Post.new(params.require(:post).permit(:title, :content))  
+		if @post.save   
+			redirect_to posts_path,:notice => "Successfully created!"  
+		else   
+			render "new"  
+		end
 	end
 	def edit
 	end 
